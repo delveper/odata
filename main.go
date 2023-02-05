132367b2612b7f9d57e1efcbc148ac2d45e53969
delveper <yevhen@bilyk.dev>
2023-02-05 13:00:48 +0200

    feat: finished

--- main.go
+++ main.go
@@ -1,42 +1,34 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"reflect"
 	"regexp"
+	"strconv"
 	"strings"
-	"time"
-	"unicode"
 )
 
-type Book struct {
-	ID        string `json:"id" sql:"id"`
-	AuthorID  string `json:"author_id" sql:"author_id"`
-	Title     string `json:"title" sql:"title"`
-	Genre     string `json:"genre" sql:"genre"`
-	Rate      int    `json:"rate" sql:"rate"`
-	Size      int    `json:"size" sql:"size"`
-	CreatedAt time.Time
-}
+const (
+	filter  = "$filter"
+	orderBy = "$orderby"
+	top     = "$top"
+	skip    = "$skip"
+)
 
-func main() {
-	var src Book
-	q, err := getStructFieldNamesAndTags(src)
-	if err != nil {
-		log.Fatal(err)
-	}
-	// q := parseQueryOprion("$filter=Rate lt 100 and Rate ge 400 and Genre eq 'Thriller'&$orderby=Name desc&$top=100&$skip=10", filter)
-	fmt.Println(q)
-}
+const defaultTagName = "sql"
+
+var ErrInvalidQuery = errors.New("invalid OData query")
 
 // DataFilter build on top of OData filter query options:
-// $filter. represents filter which supports operations: `and`, `or`, `eq`, `ne`, `gt`, `lt`.
+// $filter. represents filter which supports operations: `and`, `or`, `eq`, `ne`, `gt`, `lt`, `gte`, `lte`.
 // Not yet supports following properties: `from`, `to` (in UTC format), `in` Sequences (ids of sequences).
 // $orderby. optional param, represents sorting column which supports `acs` and `desc` operators.
 // $top. optional param, represents limit of items from the resource.
 // $skip. optional param, represents offset of records in the resource.
-// Example: /books?$filter=Rate lt 100 and Rate ge 400 and Genre eq 'Thriller'&$orderby=Name desc&$top=100&$skip=10
+// Names of fields MUST correspond to struct field names.
+// Example: /books?$filter=Rate lt 100 and Rate gte 400 and Genre eq 'Thriller'&$orderby=Title desc&$top=100&$skip=10
 type DataFilter struct {
 	Filter  *Filter
 	OrderBy *string
@@ -44,58 +36,265 @@ type DataFilter struct {
 	Skip    *int
 }
 
+// Filter represent linked lists of OData expressions.
 type Filter struct {
-	Head *Node
+	Head *FilterNode
 }
 
-type Node struct {
+// FilterNode represents OData expression.
+type FilterNode struct {
 	Field       string
-	Operator    Operator
-	Conjunction Conjunction
-	Value       any
-	Next        *Node
+	Operator    string
+	Conjunction string
+	Value       string
+	Next        *FilterNode
 }
 
-type QueryOption string
-type Operator string
-type Conjunction string
+type fieldData map[string]string
 
-const (
-	filter  QueryOption = "$filter"
-	orderBy             = "$orderby"
-	top                 = "$top"
-	skip                = "$skip"
-)
-const (
-	in Operator = "IN"
-	eq          = "="
-	ne          = "!="
-	gt          = ">"
-	lt          = "<"
-)
+// Insert adds new expression to Filter chain.
+func (f *Filter) Insert(new *FilterNode) {
+	if f.Head == nil {
+		f.Head = new
+		return
+	}
 
-const (
-	and Conjunction = "AND"
-	or              = "OR"
-)
-const defaultTagName = "sql"
+	node := f.Head
+	for node.Next != nil {
+		node = node.Next
+	}
 
-func ParseFilter() {
-	fields := strings.Join(nil, "|")
+	node.Next = new
+}
 
-	pattern := fmt.Sprintf(`(?P<field>%s)\s+(?P<operator>%s)\s+(?P<value>\d+|'[^']+\')\s*(?P<conjunction>%s)*\s*`,
-		fields,
-		`in|eq|ne|gt|lt`,
-		`and|or`,
+// ParseURL parse URL to OData filter friendly format.
+func (f *DataFilter) ParseURL(url string, src any) error {
+	data, err := getStructFieldData(src)
+
+	filter, err := parseFilter(url, data)
+	if err != nil {
+		return err
+	}
+
+	orderBy, err := parseOrderBy(url, data)
+	if err != nil {
+		return err
+	}
+
+	top, err := parseTop(url)
+	if err != nil {
+		return err
+	}
+
+	skip, err := parseSkip(url)
+	if err != nil {
+		return err
+	}
+
+	f.Filter = filter
+	f.OrderBy = orderBy
+	f.Top = top
+	f.Skip = skip
+
+	return nil
+}
+
+func (f *DataFilter) EvaluateQuery() string {
+	var query string = "WHERE "
+
+	eval := func(node *FilterNode) string {
+		return fmt.Sprintf("%v%v%v %v ",
+			node.Field,
+			node.Operator,
+			node.Value,
+			node.Conjunction,
+		)
+	}
+
+	node := f.Filter.Head
+	for node.Next != nil {
+		query += eval(node)
+		node = node.Next
+	}
+
+	query += eval(node)
+
+	if f.OrderBy != nil {
+		query = fmt.Sprintf("%v\nORDER BY %v", query, *f.OrderBy)
+	}
+
+	if f.Skip != nil {
+		query = fmt.Sprintf("%v\nOFFSET %v", query, *f.Skip)
+	}
+
+	if f.Top != nil {
+		query = fmt.Sprintf("%v\nLIMIT %v", query, *f.Top)
+	}
+
+	return query
+}
+
+// parseQueryOption parses value of given QueryOption from URL query parameters.
+func parseQueryOption(query, opt string) string {
+	pattern := fmt.Sprintf(`(?P<option>\%s=)(?P<value>[^&$]*)`, opt)
+	if match := regexp.MustCompile(pattern).
+		FindStringSubmatch(query); match != nil {
+		return match[2]
+	}
+
+	return ""
+}
+
+func parseSkip(url string) (*int, error) {
+	query := parseQueryOption(url, skip)
+	if query == "" {
+		return nil, nil
+	}
+
+	val, err := strconv.Atoi(query)
+	if err != nil {
+		return nil, fmt.Errorf("error parsing skip query option: %w", err)
+	}
+
+	return &val, nil
+}
+
+func parseTop(url string) (*int, error) {
+	query := parseQueryOption(url, top)
+	if query == "" {
+		return nil, nil
+	}
+
+	val, err := strconv.Atoi(query)
+	if err != nil {
+		return nil, fmt.Errorf("error parsing top query option: %w", err)
+	}
+
+	return &val, nil
+}
+
+func parseOrderBy(url string, fieldMap fieldData) (*string, error) {
+	query := parseQueryOption(url, orderBy)
+	if query == "" {
+		return nil, nil
+	}
+
+	sortMap := map[string]string{
+		"asc":  "ASC",
+		"desc": "DESC",
+		"ASC":  "ASC",
+		"DESC": "DESC",
+	}
+
+	var fieldList, sortList []string
+
+	for k, v := range fieldMap {
+		fieldList = append(fieldList, k, v)
+	}
+
+	for k, v := range sortMap {
+		sortList = append(sortList, v, k)
+	}
+
+	pattern := fmt.Sprintf(`(%s)(\s(%s))*,*`,
+		strings.Join(fieldList, "|"),
+		strings.Join(sortList, "|"),
 	)
 
-	_ = pattern
+	re := regexp.MustCompile(pattern)
+
+	if match := re.ReplaceAllLiteralString(query, ""); strings.TrimSpace(match) != "" {
+		return nil, fmt.Errorf("query does not correspond pattern: %s: %w", pattern, ErrInvalidQuery)
+	}
+
+	for k, v := range fieldMap {
+		query = strings.Replace(query, k, v, -1)
+	}
+
+	for k, v := range sortMap {
+		query = strings.Replace(query, k, v, -1)
+	}
+
+	return &query, nil
 }
 
-// getStructFieldNamesAndTags retrieves list of struct field names
+func parseFilter(url string, fieldMap fieldData) (*Filter, error) {
+	query := parseQueryOption(url, filter)
+	if query == "" {
+		return nil, nil
+	}
+
+	log.Printf("`%v`", query)
+
+	operMap := map[string]string{
+		"eq":  "=",
+		"ne":  "!=",
+		"gt":  ">",
+		"lt":  "<",
+		"lte": "<=",
+		"gte": ">=",
+	}
+
+	conjMap := map[string]string{
+		"and": "AND",
+		"or":  "OR",
+	}
+
+	var operList, conjList, fieldList []string
+
+	for k, v := range fieldMap {
+		fieldList = append(fieldList, k, v)
+	}
+
+	for k := range operMap {
+		operList = append(operList, k)
+	}
+
+	for k := range conjMap {
+		conjList = append(conjList, k)
+	}
+
+	pattern := fmt.Sprintf(`(?P<field>%s)\s+(?P<operator>%s)\s+(?P<value>\d+|'[^']+')\s*(?P<conjunction>%s)*\s*`,
+		strings.Join(fieldList, "|"),
+		strings.Join(operList, "|"),
+		strings.Join(conjList, "|"),
+	)
+
+	re := regexp.MustCompile(pattern)
+
+	if match := re.ReplaceAllLiteralString(query, ""); strings.TrimSpace(match) != "" {
+		return nil, fmt.Errorf("query does not correspond pattern: %s: %w", pattern, ErrInvalidQuery)
+	}
+
+	matches := re.FindAllStringSubmatch(query, -1)
+	groups := re.SubexpNames()
+
+	var fil = new(Filter)
+	for _, match := range matches {
+		var node FilterNode
+		for i := 1; i < len(groups); i++ {
+			switch groups[i] {
+			case "field":
+				node.Field = fieldMap[match[i]]
+			case "operator":
+				node.Operator = operMap[match[i]]
+			case "value":
+				node.Value = match[i]
+			case "conjunction":
+				node.Conjunction = conjMap[match[i]]
+			}
+		}
+
+		fil.Insert(&node)
+	}
+
+	return fil, nil
+}
+
+// getStructFieldData retrieves list of struct field names
 // and their tag according to given tag name.
-func getStructFieldNamesAndTags(src any) ([]string, error) {
-	var res []string
+func getStructFieldData(src any) (fieldData, error) {
+	var res = make(map[string]string, 0)
 
 	srcValue := reflect.Indirect(reflect.ValueOf(src))
 	if srcType := srcValue.Kind(); srcType != reflect.Struct {
@@ -106,45 +305,34 @@ func getStructFieldNamesAndTags(src any) ([]string, error) {
 	for i := 0; i < srcValue.NumField(); i++ {
 		fieldValue := srcValue.Field(i)
 		fieldName := srcValue.Type().Field(i).Name
+		// add only exported fields.
+		if r := string(fieldName[0]); r >= `a` && r <= `z` {
+			continue
+		}
 
 		tag := srcValue.Type().Field(i).Tag
 		tagValue := tag.Get(defaultTagName)
-
-		// add only exported fields.
-		if !unicode.IsUpper([]rune(fieldName)[0]) {
+		// add only field with tags.
+		if tagValue == "" {
 			continue
 		}
 
 		// add FieldName and value of defaultTagName.
-		res = append(res, fieldName, tagValue)
+		res[fieldName] = tagValue
 
 		// recursive call for nested structs.
 		if fieldValue.Type().Kind() != reflect.Struct {
 			continue
 		}
 
-		nested, err := getStructFieldNamesAndTags(fieldValue.Interface())
+		nested, err := getStructFieldData(fieldValue.Interface())
 		if err != nil {
 			return nil, fmt.Errorf("error validating nested struct: %w", err)
 		}
-
-		res = append(res, nested...)
+		for k, v := range nested {
+			res[k] = v
+		}
 	}
 
 	return res, nil
-}
-
-// parseQueryOption parses value of given QueryOption from URL query parameters.
-func parseQueryOption(query string, opt QueryOption) string {
-	/*
-		query := url.Query()
-		return query.Get(string(opt))
-	*/
-	pattern := fmt.Sprintf(`(?P<option>\%s=)(?P<value>[^&$]*)`, opt)
-	if match := regexp.MustCompile(pattern).
-		FindStringSubmatch(query); match != nil {
-		return match[2]
-	}
-
-	return ""
 }
